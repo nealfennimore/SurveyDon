12482a248ca31c79870bc3d366ed502bc2380fcf
nealfennimore <fennimoren@gmail.com>
2014-05-23 12:00:45 -0400

    Added basic migration tables

--- db/migrate/20140523114248_create_questions.rb
+++ db/migrate/20140523114248_create_questions.rb
@@ -1,4 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table :questions do |t|
+      t.string :question
+      t.belongs_to :survey
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20140523114309_create_choices.rb
+++ db/migrate/20140523114309_create_choices.rb
@@ -1,4 +1,10 @@
 class CreateChoices < ActiveRecord::Migration
   def change
+    create_table :choices do |t|
+      t.string :choice
+      t.belongs_to :question
+
+      t.timestamps
+    end
   end
 end
