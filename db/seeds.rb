8cd00fcc2482ae0129974d0c8574053d830b6382
Git to da choppa & Sahan Pitigala <pair+alexanderdavidpan.spitigala@devbootcamp.com>
2014-05-23 23:28:06 -0400

    Updated seed file to automatically create user accounts for everyone in our project group.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
-User.create
+User.create(username: "alex", password: "yolo")
+User.create(username: "sahan", password: "yolo")
+User.create(username: "nirav", password: "yolo")
+User.create(username: "neal", password: "yolo")
+User.create(username: "mario", password: "yolo")
 
-Survey.create(user_id: 1)
+# Survey.create(user_id: 1)
 
-Question.create(survey_id: 1)
+# Question.create(survey_id: 1)
 
-Choice.create(question_id: 1)
+# Choice.create(question_id: 1)
 
-Response.create(choice_id: 1, surveyed_user_id: 1)
+# Response.create(choice_id: 1, surveyed_user_id: 1)
 
-SurveyedUser.create(user_id: 1, response_id: 1)
+# SurveyedUser.create(user_id: 1, response_id: 1)
