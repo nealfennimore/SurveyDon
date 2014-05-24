13acf9ddda475e2acdf5549ccfcce0239662af57
Git to da choppa <alexanderdavidpan@gmail.com>
2014-05-24 19:44:05 -0400

    Added association between surveyed_users and surveys in migration file

--- db/migrate/20140523114340_create_surveyed_users.rb
+++ db/migrate/20140523114340_create_surveyed_users.rb
@@ -3,6 +3,7 @@ class CreateSurveyedUsers < ActiveRecord::Migration
     create_table :surveyed_users do |t|
       t.belongs_to :user
       t.belongs_to :response
+      t.belongs_to :survey
 
       t.timestamps
     end
